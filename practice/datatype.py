193b893f50f470b4bc4f3a375067dffdb1fca7af
Dhruvin5792 <dhruvin7640virani@gmail.com>
2025-08-13 11:29:06 +0530

    init

---
+++ practice/datatype.py
@@ -0,0 +1,56 @@
+#============= NUMERIC DATATYPE=====================
+
+#int 
+a=7640
+print(type(a))
+
+#float
+b=76.40
+print(type(b))
+
+#complex
+c=76j+23
+print(type(c))
+
+#========SEQUENCE DATATYPE ===========
+
+#str
+name='dhruvin'
+print(type(name))
+
+#list
+number=["1","2","3","7640","5229","1532"]
+print(type(number))
+
+#tuple
+age=("23","12","45","76")
+print(type(age))
+
+#boolean
+t=True
+print(type(t))
+
+f=False
+print(type(f))
+
+#range function
+
+#set  duplicate remove kari nakhe
+g={"dhruvin","viren","mayur","himansu","mayur"}
+print(type(g))
+
+#map (dict)
+v={"name":"dhruvin","work":"python"}
+print(type(v))
+
+
+a=7640
+b=int(a)
+print(type(b))
+
+
+
+
+
+
+
