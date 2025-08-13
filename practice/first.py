7830bcd8887979ec036fca5d98b32c9097676815
Dhruvin5792 <dhruvin7640virani@gmail.com>
2025-08-13 10:57:44 +0530

    init

--- practice/first.py
+++ practice/first.py
@@ -2,3 +2,16 @@ print("hello dhruvin")
 print("tops tech")
 
 print("hello ' python")
+
+print('hello \\" python')
+
+print("helo" , "python" , "hello" , "java" , sep=" & ")
+
+print("hello" , end="   ")
+print("python")
+
+print(""" hello 
+      dhruvin virani
+      how are you!!!""")
+
+print("hello","dhruvin","hello","viren")
