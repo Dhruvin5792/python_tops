267533672542eb97e6b42f55efe7cd22c6d3a343
Dhruvin5792 <dhruvin7640virani@gmail.com>
2025-08-14 09:42:25 +0530

    init

---
+++ assignment/3.4userinput.py
@@ -0,0 +1,6 @@
+name=input("Enter Your Name :-")
+age= input("Enter Your Age :-")
+
+
+print("Your Name is :-",name)
+print("Your Age is :-",age)
---
+++ assignment/3.5type.py
@@ -0,0 +1,21 @@
+num=7640     #int
+price=79.99   #float
+sum=45j+78     #complex
+name='dhruvin virani'     #str
+subject=["maths","stat","eco","english"]    #list
+mark=("34","67","86","9")     #tuple
+bol=True    #boolean
+sub={"python","php","html","softskill"}   #set
+intro={"name":"dhruvin","rollnum":"101"}  #dict
+
+
+print(type(num))
+print(type(price))
+print(type(sum))
+print(type(name))
+print(type(subject))
+print(type(mark))
+print(type(bol))
+print(type(sub))
+print(type(intro))
+
