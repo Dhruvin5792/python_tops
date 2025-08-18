0ff171e7176139bbf47be027573b60b803be014a
Dhruvin5792 <dhruvin7640virani@gmail.com>
2025-08-18 12:11:16 +0530

    init

---
+++ extra/mark.py
@@ -0,0 +1,15 @@
+mark = int(input("Enter Your Mark :-"))
+print("Your mark Is:-",mark)
+
+if mark<=100 and mark>=91:
+    print("grade is A")
+elif mark<=90 and mark>=71:
+    print("grade is B")
+elif mark<=70 and mark>=51:
+    print("grade is C")
+elif mark<=50 and mark>=35:
+    print("grade is  D")
+elif mark<=35 and mark>=0:
+    print("grade is  E")
+else:
+    print("invalid input")
